bc6d2140e4e91f34c40de57c54039bbc738f4f96
App Academy Student <student@appacademy.io>
2017-08-11 13:59:48 -0700

    hashes right...

--- LRU/lib/p02_hashing.rb
+++ LRU/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    hash = 0
+
+    self.each_with_index do |el, idx|
+      hash += (el.hash + idx.hash) % 20
+    end
+
+    hash
   end
 end
 
 class String
   def hash
+    new_hash = 0
+    self.chars.each_with_index do |ch, i|
+      new_hash += (ch.ord + i).hash
+    end
+    new_hash
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    new_hash = 0
+    self.each do |k, v|
+      new_hash += (k.to_s.ord + v.ord)
+    end
+    new_hash
   end
 end
